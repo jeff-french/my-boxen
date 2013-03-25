52df6315f171fe59d516bbce8d4324e31c4e01cd
Jeff French <jeff.french@moonswitch.com>
2013-03-24 23:22:51 -0500

    Move personal stuff to site manifest.

--- manifests/site.pp
+++ manifests/site.pp
@@ -89,4 +89,35 @@ node default {
     ensure => link,
     target => $boxen::config::repodir
   }
+  
+  # My Stuff since the personal module doesn't seem to be working :(
+  include iterm2::dev
+  include skype
+  include hipchat
+  include chrome
+  include sublime_text_2
+  include wunderlist
+  include spoitify
+  include dropbox
+
+  $home     = "/Users/${::luser}"
+  $my       = "${home}/my"
+  $dotfiles = "${my}/dotfiles"
+
+  repository { $dotfiles:
+    source  => 'jeff-french/dotfiles',
+    require => File[$my]
+  }
+
+  file { "${home}/.bash_colors":
+    ensure => link,
+  	target => "${dotfiles}/.bash_colors",
+  	require => Repository[$dotfiles]
+  }
+
+  file { "${home}/.bash_profile":
+  	ensure => link,
+  	target => "${dotfiles}/.bash_profile",
+  	require => Repository[$dotfiles]
+  }
 }
